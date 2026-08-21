6e2f8116ff7f85e2f89a1f950bfb848f44d3509c
Igor Stein <123517437+IgorEmmelStein@users.noreply.github.com>
2026-08-21 16:01:08 -0300

    Uploaded images to public

---
+++ public/uploads/stock/aaaImages.js
@@ -0,0 +1,98 @@
+const stockImages = [
+  {
+    src: "/src/assets/stock/stock_Cockpit_plane-ground_jose-lebron-hJijNmuwh7g-unsplash.jpg",
+    alt: "Aircraft cockpit on the ground by Jose Lebron",
+  },
+  {
+    src: "/src/assets/stock/stock_cockpit-commercial_shandell-venegas-t48j_2p6kxo-unsplash.jpg",
+    alt: "Commercial aircraft cockpit by Shandell Venegas",
+  },
+  {
+    src: "/src/assets/stock/stock_Cockpit-stick_leonardo-rubbiani-ZqxCPDg3GZY-unsplash.jpg",
+    alt: "Cockpit control stick by Leonardo Rubbiani",
+  },
+  {
+    src: "/src/assets/stock/stock_pilot-window_pasqualino-capobianco-3_zFKZGxWzs-unsplash.jpg",
+    alt: "Pilot looking out aircraft window by Pasqualino Capobianco",
+  },
+  {
+    src: "/src/assets/stock/stock_Pilots-cockpit_pasqualino-capobianco-he1PuIA6-UY-unsplash.jpg",
+    alt: "Pilots in cockpit by Pasqualino Capobianco",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-airport_ern-low-QMAhB9P0RXg-unsplash.jpg",
+    alt: "Commercial airplane at airport by Ern Low",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-departing_nick-morales-BwYcH78rcpI-unsplash.jpg",
+    alt: "Commercial airplane departing by Nick Morales",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-flying_meghan-gordon-MVW9JBTY7_M-unsplash.jpg",
+    alt: "Commercial airplane flying by Meghan Gordon",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-flying_rama-krushna-behera-8Likay_xccc-unsplash.jpg",
+    alt: "Airplane flying by Rama Krushna Behera",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-flying-inclined-clouds_hieu-7gJyHcCStT0-unsplash.jpg",
+    alt: "Commercial airplane inclined in clouds by Hieu",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-flying-sunset_junho-VOAYSE7EaXs-unsplash.jpg",
+    alt: "Commercial airplane flying at sunset by Junho",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-commercial-flying-up_gabriele-merlino-1kbFS4MVTeU-unsplash.jpg",
+    alt: "Airplane climbing after takeoff by Gabriele Merlino",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-departing-field_giuliano-gastaldi-xOREB60lgYw-unsplash.jpg",
+    alt: "Airplane departing from field by Giuliano Gastaldi",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-flying_mathias-reding-qYcDyPilTn0-unsplash.jpg",
+    alt: "Airplane flying by Mathias Reding",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-ground_john-waco-jr-BL_X4W-oQLc-unsplash.jpg",
+    alt: "Airplane on ground by John Waco Jr",
+  },
+  {
+    src: "/src/assets/stock/stock_plane-ground-pilot_steward-masweneng-n9By7oBszM0-unsplash.jpg",
+    alt: "Pilot standing by aircraft on ground by Steward Masweneng",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-ground-road_oleksandr-brovko-52duyrkyEls-unsplash.jpg",
+    alt: "Airplane on ground near road by Oleksandr Brovko",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-ground-sunset_roland-denes-MsZEJYU8alE-unsplash.jpg",
+    alt: "Airplane on ground at sunset by Roland Denes",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-pilot-ground-standing_maria-teneva-m2MIMI7VS4Y-unsplash.jpg",
+    alt: "Pilot standing near airplane by Maria Teneva",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-pilot-helice_luciano-faiolo-W68nUftntf4-unsplash.jpg",
+    alt: "Pilot near aircraft propeller by Luciano Faiolo",
+  },
+  {
+    src: "/src/assets/stock/stock_Plane-wing-flying-commercial-airport_miguel-angel-sanz-yXE0zybDr-I-unsplash.jpg",
+    alt: "Commercial airplane wing view by Miguel Angel Sanz",
+  },
+  {
+    src: "/src/assets/stock/stock_Planes-ground_aidan-tottori-phvVorGReGE-unsplash.jpg",
+    alt: "Multiple airplanes on ground by Aidan Tottori",
+  },
+  {
+    src: "/src/assets/stock/stock_planes-ground-private-airplanes-field.jpg",
+    alt: "Private airplanes parked on field",
+  },
+  {
+    src: "/src/assets/stock/stock_student-cockpit_truman-talbot-uj4V299Eo5c-unsplash.jpg",
+    alt: "Student pilot in cockpit by Truman Talbot",
+  },
+];
